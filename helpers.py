aa267dbe4200e7d64f3e7a09e3bd5850657a9dc4
comradeshawty <ism3@rice.edu>
2025-02-25 01:50:39 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import ast
+import numpy as np
 import geopandas as gpd
+from collections import Counter
+from scipy.spatial import cKDTree
+from shapely.geometry import Point
 import numpy as np
 import json
 from cbgs_processor import process_cbg_data_v2,compute_weighted_and_simple_median_distance
-
+from recategorize_patterns import SUB_CATEGORY_MAPPING,sub_categories_to_pretty_names,merge_duplicate_pois,update_mp,update_category,assign_place_category_and_subcategory,assign_specific_subcategories
 def load_data():
     mp=pd.read_csv('/content/drive/MyDrive/data/mp.csv')
     mp=mp[mp['PLACEKEY']!='222-222@8gk-tdk-q2k']
@@ -13,7 +18,11 @@ def load_data():
     mp=get_time_buckets(mp)
     mp=add_raw_visit_counts(mp)
     mp = propagate_month_adjustments_to_daily_counts(mp)
-    mp,removed_df=merge_duplicate_pois(mp)  
+    mp,removed_df=merge_duplicate_pois(mp)
+    mp=assign_place_category_and_subcategory(mp, SUB_CATEGORY_MAPPING, sub_categories_to_pretty_names)
+    mp=assign_specific_subcategories(mp)
+    mp=update_mp(mp)
+    mp = compute_weighted_and_simple_median_distance(mp, cbg_gdf)
     return mp, cbg_gdf
 def add_raw_visit_counts(mp):
     mp.loc[:, 'popularity_by_hour_sum'] = mp['POPULARITY_BY_HOUR'].apply(lambda x: sum(literal_eval(x)) if isinstance(x, str) else sum(x))
@@ -55,51 +64,3 @@ def drop_outliers_by_row(df, column_to_filter, k=1.5, verbose=True):
     df_out = label_outliers_iqr(df_out, column_to_filter=column_to_filter, k=k, verbose=verbose)
     df_out = df_out[~df_out['outlier']]
     return(df_out)
-
-def merge_duplicate_pois(mp, save_path="/content/drive/MyDrive/data/removed_duplicate_pois.csv"):
-    category_list=['Accounting, Tax Preparation, Bookkeeping, and Payroll Services',
-       'Depository Credit Intermediation',
-       'Agencies, Brokerages, and Other Insurance Related Activities',
-       'Insurance Carriers', 'Legal Services',
-       'Activities Related to Real Estate',
-       'Activities Related to Credit Intermediation',
-       'Offices of Real Estate Agents and Brokers',
-       'Nondepository Credit Intermediation',
-       'Other Financial Investment Activities','Offices of Other Health Practitioners', 'Offices of Physicians','General Medical and Surgical Hospitals',
-      'Outpatient Care Centers', 'Medical and Diagnostic Laboratories',
-       'Specialty (except Psychiatric and Substance Abuse) Hospitals',
-       'Other Ambulatory Health Care Services', 'Offices of Dentists',
-       'All Other Ambulatory Health Care Services']
-    mp["POLYGON_ID"] = mp["PLACEKEY"].str.split("@").str[1]
-    mp["VISITOR_HOME_CBGS_STR"] = mp["VISITOR_HOME_CBGS"].astype(str)
-    mp=mp.sort_values(by='RAW_VISIT_COUNTS',ascending=False)
-    grouped = mp.groupby(["POLYGON_ID", "VISITOR_HOME_CBGS_STR"])
-    merged_rows = []
-    removed_rows = []
-    for (visit_count, home_cbgs), group in grouped:
-        if len(group) >= 2:  # Only process sequences with 5+ duplicates
-            first_row = group.iloc[0].copy()  # Keep the first row's values
-
-            # Determine new LOCATION_NAME (Most common TOP_CATEGORY - Most common STREET ADDRESS)
-            most_common_top_category = Counter(group["TOP_CATEGORY"]).most_common(1)[0][0]
-            most_common_sub_category = Counter(group["SUB_CATEGORY"]).most_common(1)[0][0]
-            most_common_tag_category = Counter(group["CATEGORY_TAGS"]).most_common(1)[0][0]
-
-            most_common_address = Counter(group["STREET_ADDRESS"]).most_common(1)[0][0]
-            first_row["LOCATION_NAME"] = f"{most_common_sub_category} - {most_common_address}"
-            first_row["TOP_CATEGORY"]=most_common_top_category
-            first_row["SUB_CATEGORY"]=most_common_sub_category
-            first_row["CATEGORY_TAGS"]=most_common_tag_category
-            # Keep only the first row, discard the rest
-            merged_rows.append(first_row)
-            removed_rows.append(group.iloc[1:])  # Store dropped rows
-    if removed_rows:
-        removed_df = pd.concat(removed_rows)
-        removed_df.to_csv(save_path, index=False)
-    else:
-        removed_df = pd.DataFrame()
-    cleaned_mp = mp[~mp.index.isin(removed_df.index)]
-    cleaned_mp = pd.concat([cleaned_mp, pd.DataFrame(merged_rows)], ignore_index=True)
-    cleaned_mp.drop(columns=["POLYGON_ID", "VISITOR_HOME_CBGS_STR"], inplace=True)
-
-    return cleaned_mp, removed_df
